6deec45cab62a78962d81d7dc40988e72872d9b3
dimitrisplag <dplagiarinos@gmail.com>
2026-06-30 12:29:19 +0300

    static changes

--- mydashboard/settings.py
+++ mydashboard/settings.py
@@ -27,9 +27,9 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY_1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [' users.iee.ihu.gr', '127.0.0.1']
 
 
 # Application definition
